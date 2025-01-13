3444d58a0425831674d553a60ae7cf303e8a24ec
rhycce <4632924+rhycce@users.noreply.github.com.>
2025-01-13 12:23:32 -0500

    Fixed prettier formatting and added prettier check to workflow

--- src/xmlHandler.ts
+++ src/xmlHandler.ts
@@ -1,9 +1,9 @@
 import fs from 'fs'
-export function writeXmlToFile(content: string, path: string){
-    try{
-        fs.writeFileSync(path, content)
-        console.log(`File written successfully to path: ${path}`)
-    }catch (err){
-        console.log(`Unable to write to file: ${path}. Error message: ${JSON.stringify(err)}`)
-    }
+export function writeXmlToFile(content: string, path: string) {
+  try {
+    fs.writeFileSync(path, content)
+    console.log(`File written successfully to path: ${path}`)
+  } catch (err) {
+    console.log(`Unable to write to file: ${path}. Error message: ${JSON.stringify(err)}`)
+  }
 }
